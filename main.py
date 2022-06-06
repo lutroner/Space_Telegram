7f49d5d4f5f84dd1903138e7cc312fe0157c9a94
lutroner <102256437+lutroner@users.noreply.github.com>
2022-06-07 00:25:07 +0300

    Add endless loop to main.py

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
-import requests
-from pathlib import Path
-
 from dotenv import load_dotenv
 from os import environ
-from datetime import datetime
-import telegram
 from fetch_spacex_images import fetch_spacex_last_launch
 from fetch_apod_nasa_images import fetch_nasa_apod
 from fetch_epic_nasa_images import fetch_epic_photos, fetch_epic_dataset
-
-
+from publish_image_to_telegram import get_file_path, get_random_path, publish_random_image
+from time import sleep
 
 if __name__ == '__main__':
     load_dotenv()
     nasa_id = environ.get('NASA_ID')
     spacex_id = environ.get('SPACEX_ID')
     chat_id = environ.get('CHAT_ID')
-    fetch_spacex_last_launch(spacex_id)
-    fetch_nasa_apod(nasa_id, 5)
-    epic_dataset = fetch_epic_dataset(nasa_id)
-    fetch_epic_photos(nasa_id, quantity=5, epic_dataset=epic_dataset)
+    delay = int(environ.get('DELAY'))
+    file_path = get_file_path()
+    while True:
+        if not file_path:
+            fetch_spacex_last_launch(spacex_id)
+            fetch_nasa_apod(nasa_id, 2)
+            epic_dataset = fetch_epic_dataset(nasa_id)
+            fetch_epic_photos(nasa_id, quantity=2, epic_dataset=epic_dataset)
+            file_path = get_file_path()
+        else:
+            random_image = get_random_path(file_path)
+            publish_random_image(random_image, chat_id=chat_id)
+        sleep(delay)
+        print(file_path)
--- publish_image_to_telegram.py
+++ publish_image_to_telegram.py
@@ -1,11 +1,34 @@
 import telegram
 from dotenv import load_dotenv
+import os
 from os import environ
+import random
 
-load_dotenv()
-chat_id = environ.get('CHAT_ID')
 
-bot = telegram.Bot(token=environ.get('BOT_TOKEN'))
-print(bot.get_me())
-# bot.send_message(chat_id=chat_id, text='Hi there!?')
-bot.send_photo(photo=open('images/epic/epic_2.png', 'rb'), chat_id=chat_id)
+def get_file_path():
+    file_path_list = []
+    for path, dirs, files in os.walk('images/'):
+        for name in files:
+            file_path_list.append(os.path.join(path, name))
+    return file_path_list
+
+
+def get_random_path(file_path_list):
+    random_path = random.choice(file_path_list)
+    file_path_list.remove(random_path)
+    return random_path
+
+
+def publish_random_image(random_image, chat_id):
+    bot = telegram.Bot(token=environ.get('BOT_TOKEN'))
+    try:
+        bot.send_photo(photo=open(random_image, 'rb'), chat_id=chat_id)
+    except IndexError:
+        print('Images/ directory is empty')
+
+
+if __name__ == '__main__':
+    load_dotenv()
+    chat_id = os.environ.get('CHAT_ID')
+    random_image = get_random_path(get_file_path())
+    publish_random_image(random_image, chat_id=chat_id)
