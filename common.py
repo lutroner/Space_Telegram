be1daf3249c70da3c853dfaff0ccc4b28e9aacce
lutroner <102256437+lutroner@users.noreply.github.com>
2022-06-09 16:24:49 +0300

    Simplify define_file_extension function

--- common.py
+++ common.py
@@ -1,9 +1,11 @@
-from urllib.parse import urlsplit, unquote
-from os.path import split, splitext
+from urllib.parse import urlsplit
+from os.path import splitext
 
 USER_AGENT = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
                             "(KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
 
 
 def define_file_extension(url="https://example.com/txt/hello%20world.txt?v=9#python"):
-    return splitext(split(unquote(urlsplit(url).path))[1])[1]
+    file_path = urlsplit(url).path
+    _, file_extension = splitext(file_path)
+    return file_extension
